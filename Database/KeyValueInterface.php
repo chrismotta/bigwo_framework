f1e691014e32b2cd5387a585664ee4f280140360
krazydanny <danielspadafora@gmail.com>
2017-03-06 14:40:15 -0300

    redis fix

--- Database/KeyValueInterface.php
+++ Database/KeyValueInterface.php
@@ -8,11 +8,25 @@
 	interface KeyValueInterface extends ClientInterface
 	{
 
+		// VALUES
+
 		public function set ( $key, $value );
 
 		public function get ( $key );
 
-		public function getFromListByRange ( $key, $start = 0, $end = -1 );
+		public function exists ( $key );
+
+		public function remove ( $key );
+
+		public function increment ( $key, $val = 1 );
+
+		public function decrement ( $key, $val = 1 );
+
+
+
+		// LISTS
+
+		public function getFromListByRange ( $key, $start = 0, $stop = -1 );
 
 		public function getFromListByPosition ( $key, $pos );
 
@@ -20,17 +34,64 @@
 
 		public function prependToList ( $key, $value );
 
-		public function getListLength ( $key );		
+		public function getListLength ( $key );
 
-		public function exists ( $key );
 
-		public function remove ( $key );
+
+		// SETS
+
+		public function getSet ( $key );
+
+		public function addToSet ( $key, $value );
+
+		public function isInSet ( $key, $value );
+
+		public function removeFromSet ( $key, $value );
+
+		public function getSetLength ( $key );
+
+
+
+		// ORDERED SETS
+		
+		public function getFromSortedSetByScore( $key,  $start, $end );
+
+		public function addToSortedSet ( $key, $value );
+
+		public function isInSortedSet ( $key, $value );
+
+		public function removeFromSortedSet ( $key, $value );
+
+		public function getSortedSetLength ( $key );
+
+
+
+		// MAPS (objects)
+
+		public function setMap ( $key, array $data );
+
+		public function setMapField ( $key, $field, $value );
+
+		public function getMapField ( $key, $field );
+
+		public function mapFieldExists ( $key, $field );
+
+		public function getMap ( $key );
+
+		public function getMapFieldCount ( $key );
+
+		public function incrementMapValue ( $key, $field, $by = 1 );
+
+		public function removeMapField ( $key, $field );
+
+		public function getMapFieldLength ( $key, $field );
+
+
+
+
+		// GENERAL
 
 		public function flush ( );
-
-		public function increment ( $key, $val = 1 );
-
-		public function decrement ( $key, $val = 1 );
 
 		public function expireAt ( $key, $timestamp );
 
--- Database/Redis/Predis.php
+++ Database/Redis/Predis.php
@@ -30,9 +30,9 @@
 		}
 
 
-		public function getFromListByRange ( $key, $start = 0, $end = -1 )
+		public function getFromListByRange ( $key, $start = 0, $stop = -1 )
 		{
-			return $this->_predis->lrange( $key, $start, $end );
+			return $this->_predis->lrange( $key, $start, $stop );
 		}
 
 
@@ -112,7 +112,116 @@
 
 		public function flush ( )
 		{
+			$this->_predis->flushall();
 		}
+
+
+		public function flushDb ( )
+		{
+			$this->_predis->flushdb();
+		}		
+
+
+		public function setMap ( $key, array $data )
+		{
+			$this->_predis->hmset( $key, $data );
+		}
+
+
+		public function setMapField ( $key, $field, $value )
+		{
+			$this->_predis->hset( $key, $field, $value );	
+		}
+
+
+		public function getMapField ( $key, $field )
+		{
+			$this->_predis->hget( $key, $field );	
+		}
+
+
+		public function mapFieldExists ( $key, $field )
+		{
+			$this->_predis->hexists( $key, $field );
+		}
+
+
+		public function getMap ( $key )
+		{
+			$this->_predis->hgetall( $key );
+		}
+
+
+		public function getMapFieldCount ( $key )
+		{
+			$this->_predis->hlen( $key );
+		}
+
+
+		public function incrementMapValue ( $key, $field, $by = 1 )
+		{
+			$this->_predis->hincrby( $key, $field, $by );
+		}
+
+
+		public function removeMapField ( $key, $field )
+		{
+			$this->_predis->hget( $key, $field );
+		}
+
+
+		public function getMapFieldLength ( $key, $field )
+		{
+			$this->_predis->hstrlen( $key, $field );
+		}
+
+
+		public function getSet ( $key )
+		{
+			$this->_predis->smembers( $key );
+		}
+
+
+		public function addToSet ( $key, $value )
+		{
+			$this->_predis->sadd ( $key, $value );
+		}
+
+
+		public function isInSet ( $key, $value )
+		{
+			$this->_predis->sismember( $key, $value );
+		}
+
+
+		public function removeFromSet ( $key, $value )
+		{
+			$this->_predis->srem( $key, $value );
+		}
+
+
+		public function getSetLength ( $key )
+		{
+			$this->_predis->scard( $key );
+		}
+
+
+		public function addToSortedSet ( $key, $value )
+		{
+			$this->_predis->zadd ( $key, $value );
+		}
+
+
+		public function removeFromSortedSet ( $key, $value )
+		{
+			$this->_predis->zrem( $key, $value );
+		}
+
+
+		public function getSortedSetLength ( $key )
+		{
+			$this->_predis->zcard( $key );
+		}		
 
 	}
 	
